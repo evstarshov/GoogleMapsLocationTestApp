444ec499ab7a9eb7ac2e4af8794c88075789d251
Crocodil <evstarshov@icloud.com>
2022-05-13 20:05:48 +0300

    addMarker Button

--- GoogleMapsLocationTestApp/ViewController.swift
+++ GoogleMapsLocationTestApp/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     // MARK: IBOutlets
     
     @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var addMarkerButton: UIButton!
+    
+    
+    // MARK: Properties
+    
+    private let coordinate = CLLocationCoordinate2D(latitude: 55.753215, longitude: 37.622504)
     
     // MARK: Lifecycle methods
     
@@ -24,7 +30,13 @@ class ViewController: UIViewController {
     // MARK: IBActions
     
     @IBAction func goTo(_ sender: Any) {
-        configureMap()
+        mapView.animate(toLocation: coordinate)
+    }
+    
+    @IBAction func addMarker(sender: UIButton!) {
+        print("Placing marker")
+        let marker = GMSMarker(position: coordinate)
+        marker.map = mapView
     }
     
     // MARK: Private methods
@@ -32,19 +44,22 @@ class ViewController: UIViewController {
     private func setMapViewConstraints() {
         self.mapView.translatesAutoresizingMaskIntoConstraints = false
         self.mapView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
-        self.mapView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        self.mapView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        self.mapView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        self.mapView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
         self.mapView.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
         self.mapView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        self.mapView.addSubview(addMarkerButton)
+        self.addMarkerButton.translatesAutoresizingMaskIntoConstraints = false
+        self.addMarkerButton.backgroundColor = .systemBlue
+        self.addMarkerButton.rightAnchor.constraint(equalTo: self.mapView.rightAnchor).isActive = true
+        self.addMarkerButton.leftAnchor.constraint(equalTo: self.mapView.leftAnchor).isActive = true
+        self.addMarkerButton.bottomAnchor.constraint(equalTo: self.mapView.bottomAnchor).isActive = true
     }
     
     private func configureMap() {
-        let coordinate = CLLocationCoordinate2D(latitude: 55.753215, longitude: 37.622504)
         let camera = GMSCameraPosition.camera(withTarget: coordinate, zoom: 17)
         mapView.camera = camera
         mapView.isMyLocationEnabled = true
     }
-
-
 }
 
