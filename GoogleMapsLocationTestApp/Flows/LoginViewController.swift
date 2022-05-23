2ff045b35814e4cc8bf4728ff65e44cea2b94e91
Crocodil <evstarshov@icloud.com>
2022-05-23 13:05:48 +0300

    loginButton func refactoring

--- GoogleMapsLocationTestApp/Flows/LoginViewController.swift
+++ GoogleMapsLocationTestApp/Flows/LoginViewController.swift
@@ -30,8 +30,15 @@ class LoginViewController: UIViewController {
     // MARK: IBActions
     
     @IBAction func loginButtonTapped() {
-        guard loginTextField.text != "" && passwordTextField.text != "" else { return }
-        (users?.contains { $0.login == loginTextField.text })! && ((users?.contains { $0.password == passwordTextField.text }) != nil) ? checkLogin() : showAlert()
+        guard loginTextField.text != "" && passwordTextField.text != "" else { showAlertEmptyFields(); return }
+        guard let users = users else {
+            return
+        }
+        if users.contains(where: { $0.login == loginTextField.text && $0.password == passwordTextField.text }) {
+            checkLogin()
+        } else {
+            showAlert()
+        }
     }
     
     @IBAction func signUpButtonTapped() {
@@ -54,6 +61,13 @@ class LoginViewController: UIViewController {
     
     private func showAlert() {
         let alertVC = UIAlertController(title: "Error", message: "Login or password is incorrect!", preferredStyle: .alert)
+        let alertItem = UIAlertAction(title: "Ok", style: .cancel)
+        alertVC.addAction(alertItem)
+        present(alertVC, animated: true)
+    }
+    
+    private func showAlertEmptyFields() {
+        let alertVC = UIAlertController(title: "Error", message: "Login or password fields should not be empty!", preferredStyle: .alert)
         let alertItem = UIAlertAction(title: "Ok", style: .cancel)
         alertVC.addAction(alertItem)
         present(alertVC, animated: true)
